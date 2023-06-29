2c74f2f7ad295a027cb31fa871efaa541ab42d8f
betapdi <lecongquochan@gmail.com>
2023-06-29 23:14:02 +0700

    divide template into cpp and header files

--- CS163-A-Dictionary/Class/HashMap/HashMap.cpp
+++ CS163-A-Dictionary/Class/HashMap/HashMap.cpp
@@ -1,197 +1,117 @@
-#include <string>
-#include <vector>
-using namespace std;
+#include "HashMap.h"
 
-struct hashMod {
-    int mod = 1e9 + 7, len = 0;
-    vector <int> pw, hash;
+/*--------------------BST MAIN FUNCTIONS-------------------*/
+template <typename T>
+void BinarySearchTree<T>::print(TreeNode<T>* pRoot) {
+    queue<TreeNode<T>*> q;
+    q.push(pRoot);
+    while (q.size()) {
+        TreeNode<T>* u = q.front(); q.pop();
+        if (u == nullptr) continue;
+        if (u->data.num > 0)
+            cout << u->data.val << "\n";
+        q.push(u->pLeft);
+        q.push(u->pRight);
+    }
+}
 
-    hashMod(string s, int baseHash = 331, int p = (int)1e9 + 7) : mod(p) {
-        int n = (int)s.size();
-        len = n;
-        pw.resize(n + 5); hash.resize(n + 5);
-        pw[0] = 1;
-        for (int i = 1; i <= n; ++i) pw[i] = (1LL * pw[i - 1] * baseHash) % mod;
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::searchVal(TreeNode<T>* pRoot, int x) {
+    if (!pRoot) return nullptr;
+    if (x == pRoot->data.val) return pRoot;
+    if (x < pRoot->data.val) return searchVal(pRoot->pLeft, x);
+    return searchVal(pRoot->pRight, x);
+}
 
-        hash[0] = s[0];
-        for (int i = 1; i < n; ++i) {
-            hash[i] = (1LL * hash[i - 1] * baseHash + s[i]) % mod;
-        }
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::searchParent(TreeNode<T>* pRoot, int x) {
+    if (!pRoot) return nullptr;
+    if (x == pRoot->pLeft->data.val || x == pRoot->pRight->data.val) return pRoot;
+    if (x < pRoot->data.val) return searchParent(pRoot->pLeft, x);
+    return searchParent(pRoot->pRight, x);
+}
+
+template <typename T>
+void BinarySearchTree<T>::printInRange(TreeNode<T>* pRoot, int x, int y) {
+    // cout << pRoot << " " << x << " " << y << endl;
+    if (!pRoot) return;
+    if (!(pRoot->data.val <= x))
+        printInRange(pRoot->pLeft, x, y);
+    if (x <= pRoot->data.val && pRoot->data.val <= y && pRoot->data.num > 0)
+        cout << pRoot->data.val << " ";
+    if (!(pRoot->data.val >= y)) {
+        printInRange(pRoot->pRight, x, y);
+    }
+}
+
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::getCCA(TreeNode<T>* pRoot, int x, int y) {
+    if (x <= pRoot->data.val && pRoot->data.val <= y) return pRoot;
+    if (x > pRoot->data.val) return getCCA(pRoot->pRight, x, y);
+    return getCCA(pRoot->pLeft, x, y);
+}
+
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::closestCommonAncestor(TreeNode<T>* pRoot, int x, int y) {
+    //Assume x < y
+    if (x > y) swap(x, y);
+    if (!searchVal(pRoot, x) || !searchVal(pRoot, y)) return nullptr;
+    return getCCA(pRoot, x, y);
+}
+
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::insert(TreeNode<T>*& pRoot, Data x, int lb, int rb) {
+    if (lb == -1 && rb == -1) lb = leftBound, rb = rightBound;
+    int mid = (lb + rb) >> 1;
+    if (pRoot == nullptr) pRoot = new TreeNode<T>(mid, 0);
+
+    if (mid == x.val) {
+        pRoot->data.num += x.num;
+        return pRoot;
     }
 
-    int getHash() {
-        return hash[len - 1];
+    if (x.val < mid) return insert(pRoot->pLeft, x, lb, mid - 1);
+    return insert(pRoot->pRight, x, mid + 1, rb);
+}
+
+template <typename T>
+TreeNode <T>* BinarySearchTree<T>::insert(Data x) {
+    return insert(root, x);
+}
+
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::insert(int x) {
+    return insert(root, Data(x, 1));
+}
+
+template <typename T>
+void BinarySearchTree<T>::buildTree(vector <T>& a) {
+    for (auto& it : a) insert(root, it);
+}
+
+template <typename T>
+void BinarySearchTree<T>::buildTree(vector <int>& a) {
+    for (auto& it : a) insert(root, Data(it, 1));
+}
+
+template <typename T>
+void BinarySearchTree<T>::buildTree(vector <string>& a) {
+    for (auto& it : a) {
+        hashMod curHash = hashMod(it);
+        insert(root, Data(curHash.getHash(), 1));
     }
+}
 
-    int getHash(int l, int r) {
-        if (!l) return hash[r];
-        return (1LL * hash[r] - 1LL * hash[l - 1] * pw[r - l + 1] + 1LL * mod * mod) % mod;
-    }
-};
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::remove(TreeNode<T>*& pRoot, int val, int lb, int rb) {
+    if (lb == -1 && rb == -1) lb = leftBound, rb = rightBound;
+    int mid = (lb + rb) >> 1;
 
-struct Data {
-    int val, num;
-    vector <string> definitions;
-
-    Data(int val = 0, int num = 0) {
-        this->val = val;
-        this->num = num;
-    }
-
-    void clear() {
-        this->num = 0;
-        this->definitions.clear();
-    }
-};
-
-template <typename T> struct TreeNode {
-    T data;
-    TreeNode<T>* pLeft, * pRight;
-
-    TreeNode(T x = T()) {
-        data = x;
-        pLeft = nullptr;
-        pRight = nullptr;
-    }
-
-    TreeNode(int x, int num = 0) {
-        data = T(x, num);
-        pLeft = nullptr;
-        pRight = nullptr;
-    }
-};
-
-template <typename T> struct BinarySearchTree {
-    TreeNode <T>* root;
-    int leftBound = 0, rightBound = 1e9 + 10;
-
-    BinarySearchTree(int lb = 0, int rb = 1e9 + 10) {
-        root = new TreeNode<T>((lb + rb) >> 1, 0);
-        this->leftBound = lb;
-        this->rightBound = rb;
-    }
-
-    void clear(TreeNode<T>*& pRoot) {
-        if (!pRoot) return;
-        clear(pRoot->pLeft);
-        clear(pRoot->pRight);
-
+    if (mid == val) {
         pRoot->data.clear();
-        delete pRoot;
-        pRoot = nullptr;
-    }
-
-    /*--------------------BST MAIN FUNCTIONS-------------------*/
-
-    void print(TreeNode<T>* pRoot) {
-        queue<TreeNode<T>*> q;
-        q.push(pRoot);
-        while (q.size()) {
-            TreeNode<T>* u = q.front(); q.pop();
-            if (u == nullptr) continue;
-            if (u->data.num > 0)
-                cout << u->data.val << "\n";
-            q.push(u->pLeft);
-            q.push(u->pRight);
-        }
-    }
-
-    TreeNode<T>* searchVal(TreeNode<T>* pRoot, int x) {
-        if (!pRoot) return nullptr;
-        if (x == pRoot->data.val) return pRoot;
-        if (x < pRoot->data.val) return searchVal(pRoot->pLeft, x);
-        return searchVal(pRoot->pRight, x);
-    }
-
-    TreeNode<T>* searchParent(TreeNode<T>* pRoot, int x) {
-        if (!pRoot) return nullptr;
-        if (x == pRoot->pLeft->data.val || x == pRoot->pRight->data.val) return pRoot;
-        if (x < pRoot->data.val) return searchParent(pRoot->pLeft, x);
-        return searchParent(pRoot->pRight, x);
-    }
-
-    void printInRange(TreeNode<T>* pRoot, int x, int y) {
-        // cout << pRoot << " " << x << " " << y << endl;
-        if (!pRoot) return;
-        if (!(pRoot->data.val <= x))
-            printInRange(pRoot->pLeft, x, y);
-        if (x <= pRoot->data.val && pRoot->data.val <= y && pRoot->data.num > 0)
-            cout << pRoot->data.val << " ";
-        if (!(pRoot->data.val >= y)) {
-            printInRange(pRoot->pRight, x, y);
-        }
-    }
-
-    TreeNode<T>* getCCA(TreeNode<T>* pRoot, int x, int y) {
-        if (x <= pRoot->data.val && pRoot->data.val <= y) return pRoot;
-        if (x > pRoot->data.val) return getCCA(pRoot->pRight, x, y);
-        return getCCA(pRoot->pLeft, x, y);
-    }
-
-    TreeNode<T>* closestCommonAncestor(TreeNode<T>* pRoot, int x, int y) {
-        //Assume x < y
-        if (x > y) swap(x, y);
-        if (!searchVal(pRoot, x) || !searchVal(pRoot, y)) return nullptr;
-        return getCCA(pRoot, x, y);
-    }
-
-    TreeNode<T>* insert(TreeNode<T>*& pRoot, Data x, int lb = -1, int rb = -1) {
-        if (lb == -1 && rb == -1) lb = leftBound, rb = rightBound;
-        int mid = (lb + rb) >> 1;
-        if (pRoot == nullptr) pRoot = new TreeNode<T>(mid, 0);
-
-        if (mid == x.val) {
-            pRoot->data.num += x.num;
-            return pRoot;
-        }
-
-        if (x.val < mid) return insert(pRoot->pLeft, x, lb, mid - 1);
-        return insert(pRoot->pRight, x, mid + 1, rb);
-    }
-
-    TreeNode <T>* insert(Data x) {
-        return insert(root, x);
-    }
-
-    TreeNode<T>* insert(int x) {
-        return insert(root, Data(x, 1));
-    }
-
-    void buildTree(vector <T>& a) {
-        for (auto& it : a) insert(root, it);
-    }
-
-    void buildTree(vector <int>& a) {
-        for (auto& it : a) insert(root, Data(it, 1));
-    }
-
-    void buildTree(vector <string>& a) {
-        for (auto& it : a) {
-            hashMod curHash = hashMod(it);
-            insert(root, Data(curHash.getHash(), 1));
-        }
-    }
-
-    TreeNode<T>* remove(TreeNode<T>*& pRoot, int val, int lb = -1, int rb = -1) {
-        if (lb == -1 && rb == -1) lb = leftBound, rb = rightBound;
-        int mid = (lb + rb) >> 1;
-
-        if (mid == val) {
-            pRoot->data.clear();
-
-            if (pRoot->pLeft == nullptr && pRoot->pRight == nullptr) {
-                pRoot->data.clear();
-                delete pRoot;
-                pRoot = nullptr;
-            }
-
-            return pRoot;
-        }
-
-        if (val < mid) pRoot->pLeft = remove(pRoot->pLeft, val, lb, mid - 1);
-        else pRoot->pRight = remove(pRoot->pRight, val, mid + 1, rb);
 
         if (pRoot->pLeft == nullptr && pRoot->pRight == nullptr) {
+            pRoot->data.clear();
             delete pRoot;
             pRoot = nullptr;
         }
@@ -199,58 +119,74 @@ template <typename T> struct BinarySearchTree {
         return pRoot;
     }
 
-    TreeNode<T>* remove(int val) {
-        TreeNode<T>* curNode = searchVal(root, val);
-        if (!curNode) return nullptr;
+    if (val < mid) pRoot->pLeft = remove(pRoot->pLeft, val, lb, mid - 1);
+    else pRoot->pRight = remove(pRoot->pRight, val, mid + 1, rb);
 
-        return remove(root, val);
+    if (pRoot->pLeft == nullptr && pRoot->pRight == nullptr) {
+        delete pRoot;
+        pRoot = nullptr;
     }
 
-    /* -------------- CUSTOM FUNCTIONS --------------------- */
+    return pRoot;
+}
 
-    //Search word
-    TreeNode<T>* searchWord(string word) {
-        if (word.empty()) return nullptr;
-        hashMod curHash = hashMod(word);
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::remove(int val) {
+    TreeNode<T>* curNode = searchVal(root, val);
+    if (!curNode) return nullptr;
 
-        return searchVal(root, curHash.getHash());
-    }
+    return remove(root, val);
+}
 
-    //Get definitions
-    vector <string> getDefinitions(string word) {
-        hashMod curHash = hashMod(word);
+/* -------------- CUSTOM FUNCTIONS --------------------- */
 
-        //Not found
-        TreeNode<T>* curNode = searchVal(root, curHash.getHash());
-        if (!curNode) return vector <string>();
+//Search word
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::searchWord(string word) {
+    if (word.empty()) return nullptr;
+    hashMod curHash = hashMod(word);
 
-        return curNode->data.definitions;
-    }
+    return searchVal(root, curHash.getHash());
+}
 
-    //Insert word
-    TreeNode<T>* insert(string word) {
-        if (word.empty()) return nullptr;
-        hashMod curHash = hashMod(word);
-        return insert(root, Data(curHash.getHash(), 1));
-    }
+//Get definitions
+template <typename T>
+vector <string> BinarySearchTree<T>::getDefinitions(string word) {
+    hashMod curHash = hashMod(word);
 
-    //Insert definition
-    TreeNode<T>* insert(string word, string definition) {
-        if (word.empty()) return nullptr;
-        hashMod curHash = hashMod(word);
+    //Not found
+    TreeNode<T>* curNode = searchVal(root, curHash.getHash());
+    if (!curNode) return vector <string>();
 
-        //Auto create TreeNode if word is not found
-        TreeNode<T>* curNode = searchVal(root, curHash.getHash());
-        if (!curNode) curNode = insert(root, Data(curHash.getHash(), 1));
+    return curNode->data.definitions;
+}
 
-        curNode->data.definitions.push_back(definition);
-        return curNode;
-    }
+//Insert word
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::insert(string word) {
+    if (word.empty()) return nullptr;
+    hashMod curHash = hashMod(word);
+    return insert(root, Data(curHash.getHash(), 1));
+}
 
-    //Remove word
-    TreeNode<T>* remove(string word) {
-        if (word.empty()) return nullptr;
-        hashMod curHash = hashMod(word);
-        return root = remove(curHash.getHash());
-    }
-};
+//Insert definition
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::insert(string word, string definition) {
+    if (word.empty()) return nullptr;
+    hashMod curHash = hashMod(word);
+
+    //Auto create TreeNode if word is not found
+    TreeNode<T>* curNode = searchVal(root, curHash.getHash());
+    if (!curNode) curNode = insert(root, Data(curHash.getHash(), 1));
+
+    curNode->data.definitions.push_back(definition);
+    return curNode;
+}
+
+//Remove word
+template <typename T>
+TreeNode<T>* BinarySearchTree<T>::remove(string word) {
+    if (word.empty()) return nullptr;
+    hashMod curHash = hashMod(word);
+    return root = remove(curHash.getHash());
+}
---
+++ CS163-A-Dictionary/Class/HashMap/HashMap.h
@@ -0,0 +1,132 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+struct hashMod {
+    int mod = 1e9 + 7, len = 0;
+    vector <int> pw, hash;
+
+    hashMod(string s, int baseHash = 331, int p = (int)1e9 + 7) : mod(p) {
+        int n = (int)s.size();
+        len = n;
+        pw.resize(n + 5); hash.resize(n + 5);
+        pw[0] = 1;
+        for (int i = 1; i <= n; ++i) pw[i] = (1LL * pw[i - 1] * baseHash) % mod;
+
+        hash[0] = s[0];
+        for (int i = 1; i < n; ++i) {
+            hash[i] = (1LL * hash[i - 1] * baseHash + s[i]) % mod;
+        }
+    }
+
+    int getHash() {
+        return hash[len - 1];
+    }
+
+    int getHash(int l, int r) {
+        if (!l) return hash[r];
+        return (1LL * hash[r] - 1LL * hash[l - 1] * pw[r - l + 1] + 1LL * mod * mod) % mod;
+    }
+};
+
+struct Data {
+    int val, num;
+    vector <string> definitions;
+
+    Data(int val = 0, int num = 0) {
+        this->val = val;
+        this->num = num;
+    }
+
+    void clear() {
+        this->num = 0;
+        this->definitions.clear();
+    }
+};
+
+template <typename T> struct TreeNode {
+    T data;
+    TreeNode<T>* pLeft, * pRight;
+
+    TreeNode(T x = T()) {
+        data = x;
+        pLeft = nullptr;
+        pRight = nullptr;
+    }
+
+    TreeNode(int x, int num = 0) {
+        data = T(x, num);
+        pLeft = nullptr;
+        pRight = nullptr;
+    }
+};
+
+template <typename T> class BinarySearchTree {
+public:
+    TreeNode <T>* root;
+    int leftBound = 0, rightBound = 1e9 + 10;
+
+    BinarySearchTree(int lb = 0, int rb = 1e9 + 10) {
+        root = new TreeNode<T>((lb + rb) >> 1, 0);
+        this->leftBound = lb;
+        this->rightBound = rb;
+    }
+
+    void clear(TreeNode<T>*& pRoot) {
+        if (!pRoot) return;
+        clear(pRoot->pLeft);
+        clear(pRoot->pRight);
+
+        pRoot->data.clear();
+        delete pRoot;
+        pRoot = nullptr;
+    }
+
+    /*--------------------BST MAIN FUNCTIONS-------------------*/
+
+    void print(TreeNode<T>* pRoot);
+
+    void printInRange(TreeNode<T>* pRoot, int x, int y);
+
+    TreeNode<T>* searchVal(TreeNode<T>* pRoot, int x);
+
+    TreeNode<T>* searchParent(TreeNode<T>* pRoot, int x);
+
+    TreeNode<T>* getCCA(TreeNode<T>* pRoot, int x, int y);
+
+    TreeNode<T>* closestCommonAncestor(TreeNode<T>* pRoot, int x, int y);
+
+    TreeNode<T>* insert(TreeNode<T>*& pRoot, Data x, int lb = -1, int rb = -1);
+
+    TreeNode<T>* insert(Data x);
+
+    TreeNode<T>* insert(int x);
+
+    void buildTree(vector <T>& a);
+
+    void buildTree(vector <int>& a);
+
+    void buildTree(vector <string>& a);
+
+    TreeNode<T>* remove(TreeNode<T>*& pRoot, int val, int lb = -1, int rb = -1);
+
+    TreeNode<T>* remove(int val);
+
+    /* -------------- CUSTOM FUNCTIONS --------------------- */
+
+    //Search word
+    TreeNode<T>* searchWord(string word);
+
+    //Get definitions
+    vector <string> getDefinitions(string word);
+
+    //Insert word
+    TreeNode<T>* insert(string word);
+
+    //Insert definition
+    TreeNode<T>* insert(string word, string definition);
+
+    //Remove word
+    TreeNode<T>* remove(string word);
+};
