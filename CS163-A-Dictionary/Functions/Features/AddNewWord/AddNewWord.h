8d05fea89ec0bb374a6df41bcd096e667e55f122
Khoai Tây <77709398+phuotato@users.noreply.github.com>
2023-07-07 21:49:47 +0700

    Update CS163-A-Dictionary/Functions/Features/AddNewWord/AddNewWord.h
    Co-authored-by: Lê Thanh Lâm <137142421+lamlethanh777@users.noreply.github.com>

--- CS163-A-Dictionary/Functions/Features/AddNewWord/AddNewWord.h
+++ CS163-A-Dictionary/Functions/Features/AddNewWord/AddNewWord.h
@@ -1,7 +1,7 @@
 #pragma once
+#include <iostream>
+
 #include "../../../Class_Struct/Trie/Trie.h"
 #include "../../../Class_Struct/HashMap/HashMap.h"
-#include <vector>
-#include <string>
-#include <iostream>
+
 bool addNewWord(Trie& trie, BinarySearchTree& wordMap);
