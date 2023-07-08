32c609f21ba385cda3847229ab0917445e9641ac
duxgg <dug26062004@gmail.com>
2023-07-08 23:49:26 +0700

    feat-AD-quiz-feature
    clean code

--- CS163-A-Dictionary/Functions/Features/QuizFeature/Quiz.cpp
+++ CS163-A-Dictionary/Functions/Features/QuizFeature/Quiz.cpp
@@ -1,6 +1,8 @@
 #include"Quiz.h"
+#include<iostream>
+
 void quizChooseRightWord(BinarySearchTree wordMap) {
-    std::cout << "Game: choose the right word which has the definition among 4 random definitions\n";
+    std::cout << "Game: choose the right word which has the definition among 4 random word\n";
 
     Quiz curQuiz = wordMap.chooseRightWord();
     std::cout << "Question: " << curQuiz.question << '\n';
@@ -12,15 +14,15 @@ void quizChooseRightWord(BinarySearchTree wordMap) {
 
     std::string choice;
     std::cout << "Your choice: ";
-    std::cin >> choice;
+    getline(std::cin, choice);
 
     while (choice.size() > 1 || choice[0] > '4' || choice[0] < '1') {
-        std::cout << "Invalid Input. Try again: ";
-        std::cin >> choice;
+        std::cout << "Invalid Input.Please try again: ";
+        getline(std::cin, choice);
     }
 
     if (stoi(choice) - 1 == curQuiz.answer)std::cout << "Correct\n";
-    else std::cout << "Wrong\n";
+    else std::cout << "Incorrect\n";
 
     std::cout << "Answer is: " << curQuiz.answer << '\n';
 }
@@ -38,15 +40,15 @@ void quizChooseRightDef(BinarySearchTree wordMap) {
 
     std::string choice;
     std::cout << "Your choice: ";
-    std::cin >> choice;
+    getline(std::cin, choice);
 
     while (choice.size() > 1 || choice[0] > '4' || choice[0] < '1') {
-        std::cout << "Invalid Input. Try again: ";
-        std::cin >> choice;
+        std::cout << "Invalid Input. Please try again: ";
+        getline(std::cin, choice);
     }
 
     if (stoi(choice) - 1 == curQuiz.answer)std::cout << "Correct\n";
-    else std::cout << "Wrong\n";
+    else std::cout << "Incorrect\n";
 
     std::cout << "Answer is: " << curQuiz.answer << '\n';
 }
