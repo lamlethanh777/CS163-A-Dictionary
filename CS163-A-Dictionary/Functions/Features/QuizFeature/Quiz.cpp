e8ea7a706e7c31d7ad9c186e76f47300823eacd6
duxgg <dug26062004@gmail.com>
2023-07-08 22:16:28 +0700

    Quiz-feature

---
+++ CS163-A-Dictionary/Functions/Features/QuizFeature/Quiz.cpp
@@ -0,0 +1,52 @@
+#include"Quiz.h"
+void quizChooseRightWord(BinarySearchTree wordMap) {
+    std::cout << "Game: choose the right word which has the definition among 4 random definitions\n";
+
+    Quiz curQuiz = wordMap.chooseRightWord();
+    std::cout << "Question: " << curQuiz.question << '\n';
+    std::cout << "Choices:\n";
+
+    for (int i = 0; i < 4; ++i) {
+        std::cout << i + 1 << " : " << curQuiz.choices[i] << '\n';
+    }
+
+    std::string choice;
+    std::cout << "Your choice: ";
+    std::cin >> choice;
+
+    while (choice.size() > 1 || choice[0] > '4' || choice[0] < '1') {
+        std::cout << "Invalid Input. Try again: ";
+        std::cin >> choice;
+    }
+
+    if (stoi(choice) - 1 == curQuiz.answer)std::cout << "Correct\n";
+    else std::cout << "Wrong\n";
+
+    std::cout << "Answer is: " << curQuiz.answer << '\n';
+}
+
+void quizChooseRightDef(BinarySearchTree wordMap) {
+    std::cout << "Game: choose the right definition of a random word among 4 words\n";
+
+    Quiz curQuiz = wordMap.chooseRightDefinition();
+    std::cout << "Question: " << curQuiz.question << '\n';
+    std::cout << "Choices:\n";
+
+    for (int i = 0; i < 4; ++i) {
+        std::cout << i + 1 << " : " << curQuiz.choices[i] << '\n';
+    }
+
+    std::string choice;
+    std::cout << "Your choice: ";
+    std::cin >> choice;
+
+    while (choice.size() > 1 || choice[0] > '4' || choice[0] < '1') {
+        std::cout << "Invalid Input. Try again: ";
+        std::cin >> choice;
+    }
+
+    if (stoi(choice) - 1 == curQuiz.answer)std::cout << "Correct\n";
+    else std::cout << "Wrong\n";
+
+    std::cout << "Answer is: " << curQuiz.answer << '\n';
+}
---
+++ CS163-A-Dictionary/Functions/Features/QuizFeature/Quiz.h
@@ -0,0 +1,8 @@
+#ifndef QUIZ_H
+#define QUIZ_H
+#include "../../../Class_Struct/HashMap/HashMap.h"
+#include<iostream>
+void quizChooseRightWord(BinarySearchTree wordMap);
+void quizChooseRightDef(BinarySearchTree wordMap);
+
+#endif
