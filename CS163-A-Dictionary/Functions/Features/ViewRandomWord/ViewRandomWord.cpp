ec1670da53ab8919f2a78acaf27f1b8b2f25d264
duxgg <dug26062004@gmail.com>
2023-07-09 19:56:18 +0700

    clean code

--- CS163-A-Dictionary/Functions/Features/ViewRandomWord/ViewRandomWord.cpp
+++ CS163-A-Dictionary/Functions/Features/ViewRandomWord/ViewRandomWord.cpp
@@ -2,16 +2,12 @@
 #include"ViewRandomWord.h"
 
 void viewRandomWord(BinarySearchTree wordMap) { 
-
     TreeNode* randomWord = wordMap.randomNode();
 
     std::cout << "You have generated a random word \n";
-
     std::cout << "Word:\n"<<randomWord->data.word<<'\n';
     std::cout << "Definitions:\n";
-
     for (int i = 0; i < randomWord->data.definitions.size(); i++) {
         std::cout << randomWord->data.definitions[i] << '\n';
     }
-
 }
