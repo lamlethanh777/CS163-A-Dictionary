79977624abc6985cda336110a1a2e2eeb3752878
duxgg <dug26062004@gmail.com>
2023-07-12 20:03:12 +0700

    feat-AD-view-favorite-list
    View list of favorite word

---
+++ CS163-A-Dictionary/Functions/Features/ViewFavoriteList/ViewFavoriteList.h
@@ -0,0 +1,9 @@
+#ifndef VIEW_FAVORITE_LIST_H    
+#define VIEW_FAVORITE_LIST_H     
+
+#include <string>
+#include <vector>
+
+void viewFavoriteList(std::vector<std::string>& favoriteList);
+
+#endif // !VIEW_FAVORITE_LIST_H    
