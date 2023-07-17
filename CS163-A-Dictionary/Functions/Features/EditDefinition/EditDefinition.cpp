5adf505f4fc1100fa72b799be4982b43de0480dc
phuotato <pphuonganhwork@gmail.com>
2023-07-17 22:49:33 +0700

    edit Def - choosing action

--- CS163-A-Dictionary/Functions/Features/EditDefinition/EditDefinition.cpp
+++ CS163-A-Dictionary/Functions/Features/EditDefinition/EditDefinition.cpp
@@ -22,4 +22,58 @@ void editDefinition(Trie& trie, BinarySearchTree& wordMap) {
     for (const std::string& definition : hashMapNode->definitions) {
         std::cout << "- " << definition << '\n';
     }
+
+    std::cout << "Select an action:\n";
+    std::cout << "1. Add a definition\n";
+    std::cout << "2. Remove a definition\n";
+    std::cout << "3. Modify a definition\n";
+    int action;
+    std::cout << "Enter your choice: ";
+    std::cin >> action;
+    std::cin.ignore();  // Ignore the newline character
+
+    switch (action) {
+    case 1: {
+        std::string newDefinition;
+        std::cout << "Enter the new definition to add: ";
+        std::getline(std::cin, newDefinition);
+        hashMapNode->definitions.push_back(newDefinition);
+        std::cout << "Definition added successfully.\n";
+        break;
+    }
+    case 2: {
+        int index;
+        std::cout << "Enter the index of the definition to remove: ";
+        std::cin >> index;
+        std::cin.ignore();  // Ignore the newline character
+        if (index >= 0 && index < hashMapNode->definitions.size()) {
+            hashMapNode->definitions.erase(hashMapNode->definitions.begin() + index);
+            std::cout << "Definition removed successfully.\n";
+        }
+        else {
+            std::cout << "Invalid index.\n";
+        }
+        break;
+    }
+    case 3: {
+        int index;
+        std::cout << "Enter the index of the definition to modify: ";
+        std::cin >> index;
+        std::cin.ignore();  // Ignore the newline character
+        if (index >= 0 && index < hashMapNode->definitions.size()) {
+            std::string newDefinition;
+            std::cout << "Enter the new definition: ";
+            std::getline(std::cin, newDefinition);
+            hashMapNode->definitions[index] = newDefinition;
+            std::cout << "Definition modified successfully.\n";
+        }
+        else {
+            std::cout << "Invalid index.\n";
+        }
+        break;
+    }
+    default:
+        std::cout << "Invalid choice.\n";
+        break;
+    }
 }
