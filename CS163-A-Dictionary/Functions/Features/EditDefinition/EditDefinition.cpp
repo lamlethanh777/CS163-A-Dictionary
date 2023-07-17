c862870965a9c93772ab63e3786a48adea12ad8c
phuotato <pphuonganhwork@gmail.com>
2023-07-17 21:45:57 +0700

    Edit def find word

---
+++ CS163-A-Dictionary/Functions/Features/EditDefinition/EditDefinition.cpp
@@ -0,0 +1,22 @@
+#include "EditDefinition.h"
+
+void editDefinition(Trie& trie, BinarySearchTree& wordMap) {
+    std::string word;
+    std::cout << "Enter the word to edit its definition: ";
+    std::getline(std::cin, word);
+
+    TrieNode* trieNode = trie.searchWord(word);
+    if (trieNode == nullptr) {
+        std::cout << "Word not found.\n";
+        return;
+    }
+
+    long long hashIndex = trieNode->hashIndex;
+    TreeNode* bstNode = wordMap.searchWord(hashIndex);
+    if (bstNode == nullptr) {
+        std::cout << "Error: Word not found in the hash map.\n";
+        return;
+    }
+
+    
+}
---
+++ CS163-A-Dictionary/Functions/Features/EditDefinition/EditDefinition.h
@@ -0,0 +1,6 @@
+#pragma once
+#pragma once
+#include <iostream>
+
+#include "../../../Class_Struct/Trie/Trie.h"
+#include "../../../Class_Struct/HashMap/HashMap.h"
