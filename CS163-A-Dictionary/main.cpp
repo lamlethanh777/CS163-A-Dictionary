516eef02f61b602cb6d7096402b4555099f81b09
phuotato <pphuonganhwork@gmail.com>
2023-08-07 22:00:13 +0700

    Loading screen

--- CS163-A-Dictionary/main.cpp
+++ CS163-A-Dictionary/main.cpp
@@ -1,9 +1,11 @@
 #include "Functions/Functions.h"
 #include "Functions/Utilities/LogFunctions/LogFunctions.h"
+#include "Functions/Display/Display.h"
 
 // Testing
 int main() {
     //clearLog();
+    displayLoadingAnimation();
     menu();
 	return 0;
 }
